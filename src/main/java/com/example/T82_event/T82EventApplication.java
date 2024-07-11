04df5f2cb47e31cf0f6799f21138929a22360a1d
jungryuel <zldrktduf0927@naver.com>
2024-07-11 11:23:11 +0900

    # Feat: 프로젝트 세팅 깃에 기본 프로젝트를 올립니다

---
+++ src/main/java/com/example/T82_event/T82EventApplication.java
@@ -0,0 +1,13 @@
+package com.example.T82_event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T82EventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T82EventApplication.class, args);
+	}
+
+}
