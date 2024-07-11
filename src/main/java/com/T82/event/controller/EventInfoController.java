58f3ca364af1429647de065babcd0e52c99f1bd8
HuiSeokJeong <tobysug123@gmail.com>
2024-07-12 03:39:55 +0900

    Feat: 공연정보 수정 컨트롤러에 생성(#12) 공연정보를 수정하는 기능 생성

--- src/main/java/com/T82/event/controller/EventInfoController.java
+++ src/main/java/com/T82/event/controller/EventInfoController.java
@@ -1,12 +1,10 @@
 package com.T82.event.controller;
 
 import com.T82.event.dto.request.EventInfoRequest;
+import com.T82.event.dto.request.UpdateEventInfoRequest;
 import com.T82.event.service.EventInfoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/contents")
@@ -18,4 +16,6 @@ public class EventInfoController {
     public void createEventInfo(@RequestBody EventInfoRequest request) {
         eventInfoService.createEventInfo(request);
     }
+    @PostMapping("{eventInfoId}")
+    public void updateEventInfo(@PathVariable Long eventInfoId, @RequestBody UpdateEventInfoRequest request) {}
 }
