b8326689750ffdb1ead3eb78798e56e901081dcd
jungryuel <zldrktduf0927@naver.com>
2024-07-13 17:56:13 +0900

    REFACTOR: 오늘 날짜 이후로 가장 가까운 이벤트 정보 10개 리턴 기능 쿼리 변경

--- src/main/java/com/T82/event/domain/repository/EventInfoRepository.java
+++ src/main/java/com/T82/event/domain/repository/EventInfoRepository.java
@@ -12,6 +12,6 @@ import java.util.Optional;
 
 public interface EventInfoRepository extends JpaRepository<EventInfo, Long> {
     //현재 시간으로 예매시간이 빠른순으로 정렬
-    @Query(value = "SELECT * FROM event_infos WHERE book_start_time > : now ORDER BY book_start_time DESC LIMIT 10", nativeQuery = true)
+    @Query(value = "SELECT * FROM event_infos WHERE book_start_time > : now ORDER BY book_start_time ASC LIMIT 10", nativeQuery = true)
     List<EventInfo> findComingEvents(@Param("now") LocalDateTime now);
 }
