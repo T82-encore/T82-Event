418c8b51eabd61150db5fb3fac7bac57752ddf8f
HuiSeokJeong <tobysug123@gmail.com>
2024-07-14 23:04:47 +0900

    Test: 현재 일 기준으로 해당 카테고리 내에서 오픈이 제일 빠른 티켓 10개 리턴(#20) 선택한 카테고리 하위 티켓 중에서 오픈일이 빠른 티켓 순으로 10개 전달 테스트 케이스 추가

---
+++ src/test/java/com/T82/event/domain/repository/EventInfoRepositoryTest.java
@@ -0,0 +1,71 @@
+package com.T82.event.domain.repository;
+
+import com.T82.event.domain.EventInfo;
+import com.T82.event.dto.request.EventInfoRequest;
+import com.T82.event.dto.response.EventInfoListResponse;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+
+class EventInfoRepositoryTest {
+    @Autowired
+    private EventInfoRepository eventInfoRepository;
+    @Autowired
+    private EntityManager entityManager;
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Nested
+    @Transactional
+    class 선택한_카테고리_하위_티켓_중에서_오픈일이_빠른_티켓_10개_전달 {
+        @BeforeEach
+        void setUp() {
+            for(int l = 1; l < 5; l++) {
+                for(int i = 4; i < 10; i++) {
+                    EventInfoRequest request = new EventInfoRequest(
+                            "테스트 제목"+l+i,
+                            "테스트 내용"+l+i,
+                            "174분",
+                            "18세",
+                            LocalDateTime.of(2024, 8, l, i, 0),
+                            1L,
+                            (long) i
+                    );
+                    eventInfoRepository.save(request.toEntity());
+                }
+            }
+            entityManager.flush();
+            entityManager.clear();
+        }
+        @Test
+        void 성공() {
+            //given
+            Long categoryId = 1L;
+            LocalDateTime currentDateTime = LocalDateTime.now();
+            List<Long> categoryIds = categoryRepository.findSubCategoryIdsByParentId(categoryId);
+            //when
+            List<EventInfo> list = eventInfoRepository
+                    .findNextUpcomingEvents(
+                            categoryIds,
+                            PageRequest.of(0, 10),
+                            currentDateTime);
+            //then
+            assertTrue(list.size() <= 10 && list.size() >= 5);
+            for(int i = 0; i < list.size() - 1; i++) {
+                assertTrue(list.get(i).getBookStartTime().compareTo(list.get(i+1).getBookStartTime()) <= 0);
+            }
+        }
+    }
+}
