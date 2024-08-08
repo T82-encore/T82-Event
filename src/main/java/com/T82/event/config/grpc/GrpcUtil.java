e995b40cb9cc27e01a28d436be4703ddcc376c5e
HuiSeokJeong <tobysug123@gmail.com>
2024-08-08 16:20:32 +0900

    [feat] grpc server & util 분리

--- src/main/java/com/T82/event/config/grpc/GrpcUtil.java
+++ src/main/java/com/T82/event/config/grpc/GrpcUtil.java
@@ -1,4 +1,22 @@
 package com.T82.event.config.grpc;
 
+import com.T82.event.domain.repository.EventRepository;
+import com.T82.event.dto.response.EventDetail;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.t82.event.lib.GetEventReply;
+
+@Component
+@RequiredArgsConstructor
 public class GrpcUtil {
+    private final EventRepository eventRepository;
+
+    public GetEventReply getEventDetail(Long eventId) {
+        EventDetail eventDetail = eventRepository.findEventDetailByEventId(eventId);
+        return GetEventReply.newBuilder()
+                .setEventInfoId(eventDetail.getEventInfoId())
+                .setTitle(eventDetail.getTitle())
+                .setEventStartTime(eventDetail.getEventStartTime().toString())
+                .build();
+    }
 }
