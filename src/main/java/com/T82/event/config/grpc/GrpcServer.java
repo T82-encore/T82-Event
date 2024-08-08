1e5f297905695166a86b88aee5a77eb1b6768baf
HuiSeokJeong <tobysug123@gmail.com>
2024-08-08 16:20:32 +0900

    [feat] grpc server & util 분리

--- src/main/java/com/T82/event/config/grpc/GrpcServer.java
+++ src/main/java/com/T82/event/config/grpc/GrpcServer.java
@@ -1,7 +1,5 @@
 package com.T82.event.config.grpc;
 
-import com.T82.event.domain.repository.EventRepository;
-import com.T82.event.dto.response.EventDetail;
 import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
 import net.devh.boot.grpc.server.service.GrpcService;
@@ -12,18 +10,11 @@ import org.t82.event.lib.GetEventRequest;
 @GrpcService
 @RequiredArgsConstructor
 public class GrpcServer extends EventGrpc.EventImplBase {
-    private final EventRepository eventRepository;
+    private final GrpcUtil grpcUtil;
 
     @Override
     public void getEventDetail(GetEventRequest request, StreamObserver<GetEventReply> responseObserver) {
-        EventDetail eventDetail = eventRepository.findEventDetailByEventId(request.getEventId());
-        responseObserver.onNext(
-                GetEventReply.newBuilder()
-                        .setEventInfoId(eventDetail.getEventInfoId())
-                        .setTitle(eventDetail.getTitle())
-                        .setEventStartTime(eventDetail.getEventStartTime().toString())
-                .build()
-        );
+        responseObserver.onNext(grpcUtil.getEventDetail(request.getEventId()));
         responseObserver.onCompleted();
     }
 }
