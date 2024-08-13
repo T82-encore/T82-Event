a1e989eeb97b24b15434bd79d07a1271b56d1235
jungryuel <zldrktduf0927@naver.com>
2024-08-13 15:04:30 +0900

    Refactor: 이벤트 응답값 수정

--- src/main/java/com/T82/event/dto/response/EventInfoListResponse.java
+++ src/main/java/com/T82/event/dto/response/EventInfoListResponse.java
@@ -2,16 +2,22 @@ package com.T82.event.dto.response;
 
 import com.T82.event.domain.EventInfo;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public record EventInfoListResponse(
         Long eventInfoId,
         String title,
-        Double rating
+        Double rating,
+
+        LocalDateTime bookStartTime
         ) {
     public static EventInfoListResponse from(EventInfo eventInfo) {
         return new EventInfoListResponse(
                 eventInfo.getEventInfoId(),
                 eventInfo.getTitle(),
-                eventInfo.getRating()
+                eventInfo.getRating(),
+                eventInfo.getBookStartTime()
         );
     }
 }
